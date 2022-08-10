50427f5b702e37605497895ac17744fc9d85fb98
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-10 14:48:40 +0900

    btn클릭 시 fragment 전환하는 간단한 예제

--- AboutNavigation/app/src/main/java/com/duran/aboutnavigation/MainActivity.kt
+++ AboutNavigation/app/src/main/java/com/duran/aboutnavigation/MainActivity.kt
@@ -2,10 +2,32 @@ package com.duran.aboutnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1: Button = findViewById(R.id.btn1)
+        val btn2: Button = findViewById(R.id.btn2)
+
+        val manager = supportFragmentManager
+
+        btn1.setOnClickListener {
+            val transaction = manager.beginTransaction()
+            val fragment1 = BlankFragment1()
+            transaction.replace(R.id.frameArea, fragment1)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        btn2.setOnClickListener {
+            val transaction = manager.beginTransaction()
+            val fragment2 = BlankFragment2()
+            transaction.replace(R.id.frameArea, fragment2)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 }
