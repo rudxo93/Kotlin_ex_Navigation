22913f2d11b4954fb9f3c2203d6463f1ad71fd11
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-10 20:21:33 +0900

    Navigation의 Fragment 전환 시 애니메이션 효과 적용해보기

--- NavigationAnimation/app/src/main/java/com/duran/navigationanimation/MainActivity.kt
+++ NavigationAnimation/app/src/main/java/com/duran/navigationanimation/MainActivity.kt
@@ -3,6 +3,13 @@ package com.duran.navigationanimation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// 첫번째 Fragment - A / 두번째 Fragment - B
+
+// enterAnim - A -> B 갈 때 B가 보이는 방식
+// exitAnim - A -> B 갈 때 A가 사라지는 방식
+// popEnterAnim - 뒤로가기 B -> A로 올 때 A가 나타나는 방식
+// popExitAnim - 뒤로가기로 B -> A로 올 때 B가 사라지는 방식
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
