f860bbc6e5e82ba370139d22d340b9ce61199a66
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-10 14:51:19 +0900

    btn클릭 시 visibility 를 사용해서 보여지게 하는 방법(총 2가지의 방법이 있다.)

--- AboutNavigation/app/src/main/java/com/duran/aboutnavigation/MainActivity.kt
+++ AboutNavigation/app/src/main/java/com/duran/aboutnavigation/MainActivity.kt
@@ -2,7 +2,9 @@ package com.duran.aboutnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,19 +17,23 @@ class MainActivity : AppCompatActivity() {
         val manager = supportFragmentManager
 
         btn1.setOnClickListener {
-            val transaction = manager.beginTransaction()
+            val area1 : LinearLayout = findViewById(R.id.area1)
+            area1.visibility = View.VISIBLE
+            /*val transaction = manager.beginTransaction()
             val fragment1 = BlankFragment1()
             transaction.replace(R.id.frameArea, fragment1)
             transaction.addToBackStack(null)
-            transaction.commit()
+            transaction.commit()*/
         }
 
         btn2.setOnClickListener {
-            val transaction = manager.beginTransaction()
+            val area2 : LinearLayout = findViewById(R.id.area2)
+            area2.visibility = View.VISIBLE
+            /*val transaction = manager.beginTransaction()
             val fragment2 = BlankFragment2()
             transaction.replace(R.id.frameArea, fragment2)
             transaction.addToBackStack(null)
-            transaction.commit()
+            transaction.commit()*/
         }
     }
 }
